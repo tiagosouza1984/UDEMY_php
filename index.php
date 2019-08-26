35e16da3eea073ee9e354efb19b69a618ec54914
System Administrator <root@MacBook-Alvosoft-Tiago.local>
2019-08-26 12:46:07 -0300

    alteração fora do github

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 echo "<strong>Teste código php alterado<strong><br>
 formulário <form> 
 insira o texto aqui<br><textarea></textarea>
-</form>"
+</form>";
+echo "novo teste de alteração fora do github";
+
 ?>
